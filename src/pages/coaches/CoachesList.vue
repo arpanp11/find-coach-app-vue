14661438a7d8e0d9638aca9f65a981485273751a
arpanp11 <arpanpatel2810@gmail.com>
2020-11-25 11:47:10 -0500

    add coachitem and other base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,12 +2,53 @@
   <section>FILTER</section>
 
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register a Coach</router-link>
-    </div>
-    <ul>
-      LIST OF COACHES
-    </ul>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register a Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No Coaches found.</h3>
+    </base-card>
   </section>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
+
+export default {
+  components: { CoachItem, BaseButton },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
